2643c4b3cead4c7ef8b1be160e348aa3878df5a3
Maks Golikov <maksim.golikov01@gmail.com>
2021-02-24 14:41:01 +0200

    create table Students and change app properties

---
+++ src/main/java/com/example/demo/model/Students.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Students {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "age")
+    private int age;
+}
